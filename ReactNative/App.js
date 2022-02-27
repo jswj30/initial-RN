f5da94a8ecceb51db22ed776818dcffe09af7b7c
jswj <jswj30@naver.com>
2022-02-27 11:29:28 +0900

    1

--- ReactNative/App.js
+++ ReactNative/App.js
@@ -1,51 +1,85 @@
-import React, {useState} from 'react';
-import {View, Text, SafeAreaView, FlatList, StyleSheet} from 'react-native';
+import React, {useState, useLayoutEffect} from 'react';
+import {View, Text, SafeAreaView, FlatList, StyleSheet, ActivityIndicator} from 'react-native';
 
 const App = () => {
-  const [data, setData] = ([
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba', 
-      title: 'First', 
-      content: 'This is First Content', 
-    }, 
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63', 
-      title: 'Secone', 
-      content: 'This is Second Content', 
-    }, 
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72', 
-      title: 'Third', 
-      content: 'This is Third Content', 
-    }, 
-  ]);
+  const [data, setData] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  useLayoutEffect(() => {
+    getData();
+  }, [])
 
+  const getData = () => {
+    setIsLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/posts/')
+    .then((res) => res.json())
+    .then((res) => {
+      let result = data.concat(res.slice(offset, offset + 10));
+      setData([...result]);
+    })
+    .then(() => {
+      setOffset(offset + 10);
+      setIsLoading(false);
+    })
+    .catch((err) => {
+      console.error(err);
+      setIsLoading(false);
+    })
+  }
+  
   const renderItem = ({item}) => {
     return (
-      <View>
-        <Text>{item.title}</Text>
-        <Text>{item.content}</Text>
+      <View style={styles.renderArea}>
+        <Text>{item.id}</Text>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.content}>{item.body}</Text>
       </View>
     )
   }
 
+  const onEndReached = () => {
+    if (isLoading) {
+      return;
+    } else {
+      getData();
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <FlatList 
-          data={data} 
-          renderItem={renderItem} 
-          keyExtractor={(item) => item.id} 
-        />
-      </View>
+      <FlatList 
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        onEndReached={onEndReached}
+        onEndReachedThreshold={0.8}
+      />
     </SafeAreaView>
-  );
+  )
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    // flex: 1, 
   }, 
+  renderArea: {
+    // borderWidth: 1, 
+    paddingVertical: 15, 
+    paddingHorizontal: 20, 
+  }, 
+  title: {
+    textAlign: 'center', 
+    color: '#222222', 
+    fontSize: 20, 
+    // borderWidth: 1, 
+  }, 
+  content: {
+    color: '#222222', 
+    fontSize: 16, 
+    // borderWidth: 1, 
+  }
 })
 
 export default App;
